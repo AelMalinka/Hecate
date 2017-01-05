05e1d6d880c48755ee345d283cea8e1e0e12d421
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-01-05 00:52:41 -0500

    add ModifierList

---
+++ test/modifier/list.cc
@@ -0,0 +1,80 @@
+/*	Copyright 2017 (c) Michael Thomas (malinka) <malinka@entropy-development.com>
+	Distributed under the terms of the GNU Affero General Public License v3
+*/
+
+#include <gtest/gtest.h>
+#include "ModifierList.hh"
+#include "Stat.hh"
+#include "Skill.hh"
+
+using namespace Entropy::Hecate;
+using namespace testing;
+using namespace std;
+
+namespace {
+	struct TestStat_tag{}; using TestStat = Stat<TestStat_tag, Linear<2>>;
+	struct TestSkill_tag{}; using TestSkill = Skill<TestSkill_tag, Linear<1>, TestStat>;
+
+	ModifierType mytype1;
+	ModifierType mytype2(true);
+
+	TEST(ModifierList, Create) {
+		TestStat stat(10);
+		TestSkill skill(10, stat);
+
+		ModifierList a;
+		EXPECT_EQ(a.size(), 0);
+		EXPECT_EQ(a.Value(), 0);
+
+		ModifierList b({
+			Modifier(10),
+			Modifier(stat),
+			Modifier(skill)
+		});
+		EXPECT_EQ(b.size(), 1);
+		EXPECT_EQ(b.Value(), 40);
+
+		ModifierList c({
+			Modifier(10, mytype1),
+			Modifier(skill, mytype1),
+			Modifier(10, mytype2),
+			Modifier(10, mytype2),
+			Modifier(10, mytype1)
+		});
+		EXPECT_EQ(c.size(), 2);
+		EXPECT_EQ(c.Value(), 40);
+	}
+
+	TEST(ModifierList, Add) {
+		TestStat stat(10);
+		TestSkill skill(10, stat);
+
+		ModifierList a;
+		EXPECT_EQ(a.size(), 0);
+		EXPECT_EQ(a.Value(), 0);
+
+		a.Add(Modifier(10));
+		EXPECT_EQ(a.size(), 1);
+		EXPECT_EQ(a.Value(), 10);
+
+		a.Add(Modifier(10));
+		EXPECT_EQ(a.size(), 1);
+		EXPECT_EQ(a.Value(), 20);
+
+		a.Add(Modifier(10, mytype1));
+		EXPECT_EQ(a.size(), 2);
+		EXPECT_EQ(a.Value(), 30);
+
+		a.Add(Modifier(10, mytype1));
+		EXPECT_EQ(a.size(), 2);
+		EXPECT_EQ(a.Value(), 30);
+
+		a.Add(Modifier(10, mytype2));
+		EXPECT_EQ(a.size(), 3);
+		EXPECT_EQ(a.Value(), 40);
+
+		a.Add(Modifier(10, mytype2));
+		EXPECT_EQ(a.size(), 3);
+		EXPECT_EQ(a.Value(), 50);
+	}
+}
