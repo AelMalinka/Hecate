8209fcd0b370529f737fe366c1aa3f0c9787eef4
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-02-16 02:21:49 -0500

    Don't distribute config.h, optionally depend on __atttribute__

--- src/Random.cc
+++ src/Random.cc
@@ -4,10 +4,22 @@
 
 #include "Random.hh"
 
+#ifdef HAVE_CONFIG_H
+#	include "config.h"
+#endif
+
+#ifndef HAVE___ATTRIBUTE__
+#	define __attribute__(x)
+#endif
+
+#include <random>
+#include "pcg_random.hpp"
+
 using namespace Entropy::Hecate;
 using namespace std;
 
-pcg_extras::seed_seq_from<random_device> detail::Random::_seed;
-pcg32 detail::Random::Generator(_seed);
-uniform_int_distribution<PercentType> detail::Random::Distribution(0, 99);
-function<PercentType()> Entropy::Hecate::Roll(bind(detail::Random::Distribution, detail::Random::Generator));
+pcg_extras::seed_seq_from<random_device> _seed;
+pcg32 _generator(_seed);
+uniform_int_distribution<PercentType> _distribution(0, 99);
+
+function<PercentType()> Entropy::Hecate::Roll(bind(_distribution, _generator));
--- src/Random.hh
+++ src/Random.hh
@@ -7,31 +7,12 @@
 
 #	include "Exception.hh"
 
-#	include <random>
 #	include <functional>
-
-#	ifndef HAVE_ATTRIBUTE
-#		define __attribute__(x)
-#	endif
-
-#	include "pcg_random.hpp"
 
 	namespace Entropy
 	{
 		namespace Hecate
 		{
-			namespace detail
-			{
-				class Random
-				{
-					private:
-						static pcg_extras::seed_seq_from<std::random_device> _seed;
-					public:
-						static pcg32 Generator;
-						static std::uniform_int_distribution<PercentType> Distribution;
-				};
-			}
-
 			extern std::function<PercentType()> Roll;
 		}
 	}
